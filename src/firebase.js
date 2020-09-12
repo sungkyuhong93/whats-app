e00ab67d236e58e50b5b4be0404db5a7b221e3d0
Sung Kyu Hong <sungkyuhong93@gmail.com>
2020-09-12 12:20:05 -0400

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA5tjfMw6li7f2QBD_xpYF8_ToCXZWa3EY",
+  authDomain: "whats-app-clone-5c5b6.firebaseapp.com",
+  databaseURL: "https://whats-app-clone-5c5b6.firebaseio.com",
+  projectId: "whats-app-clone-5c5b6",
+  storageBucket: "whats-app-clone-5c5b6.appspot.com",
+  messagingSenderId: "1025827850984",
+  appId: "1:1025827850984:web:6e8bd04af21af4ef34175d",
+};
