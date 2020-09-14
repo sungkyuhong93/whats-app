7be2f337c1807ebbeb0526ea830542704f68620a
Sung Kyu Hong <sungkyuhong93@gmail.com>
2020-09-14 00:32:04 -0400

    deployed

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyA5tjfMw6li7f2QBD_xpYF8_ToCXZWa3EY",
   authDomain: "whats-app-clone-5c5b6.firebaseapp.com",
@@ -7,3 +9,11 @@ const firebaseConfig = {
   messagingSenderId: "1025827850984",
   appId: "1:1025827850984:web:6e8bd04af21af4ef34175d",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
